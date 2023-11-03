72689a5226aadfb37b121e2c28477c21c93e91f7
Dima Oliinyk <oliynykdmitry83@gmail.com>
2023-11-03 11:28:24 +0000

    Some updates to the file

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ file.close()
 
 """
 
-#$ rm names.txt // Removes file //   works in CLI like built-in terminal or any other shells
+#$ rm names.txt // Removes file // works in CLI like built-in terminal or any other shells
 
 """
 name = input("What is your name? ")
@@ -55,13 +55,14 @@ with open("names.txt", "a") as file:
 
 # reading a file:
 
+"""
 with open("names.txt", "r") as file:
   lines = file.readlines() 
   # readlines() - reads all lines of the file and return it as a list
   # rstrip() - removes whitespaces from the end of each line
   # split() - splits each line into a list of words
   # you can find ALL built in funcs in docs.python.org or other alterniative resources
-  # e.g. w3school or so...
+  # but
 
 for line in lines:
   print("Hello, ",  line)
@@ -69,6 +70,50 @@ for line in lines:
 # to rid of an empty line at the end of each line(after each Name)
 # you can pass the third arugment to print() as end=""
 # or you can use rstrip() to remove whitespaces from the end of each line
+
+"""
+# you can rewrite this code in a little bit different/"Pythonic"/more compact way:
+
+"""
+with open("names.txt", "r") as file:  # open file for reading
+  for line  in file:                  # iterativly read each line
+    print("Hello, ", line.rstrip())   # print each line and remove whitespaces
+
+"""
+
+#  to sort data from this file we need to read it first, then put in a list and sort it
+#  note! that we are fetching data from a file and put everything into a variable and 
+#  manupulating it
+
+"""
+names = []  #  it's quite a common technicue to fech data, put it into a temprorary
+            #  varible and using certain features of the type of varible we can
+            #  manipulate it
+
+with open("names.txt") as file:
+  for line in file:
+    names.append(line.rstrip())
+
+for name in sorted(names):    # sorting the list before itarating over it
+  print(f"Hello, {name}")  
+
+
+"""
+
+# we can do it in a more compact way:
+
+"""
+with open("names.txt") as file:
+  for line in sorted(file):
+    print("Hello,", line.rstrip())
+
+"""
+
+#  BUT in this case YOU have to answer a question:
+#  Do we actially want to make changes to the data we are iterating over?
+
+#  docs.python.org/3/library/function.html#sorted
+#  here you can find all pissible ways of sorting 
+
+
   
-# if you want to print the whole file at once:
-                        
